bc7bce952ac4c3ba4021642a42f9d8bfaf48d828
AlexandraDuron <alexandra,oduron@gmail.com>
2021-11-26 11:48:15 +0000

    hackathon | added getters and setters

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -15,6 +15,52 @@ public class Order {
         this.productValue=productValue;
     }
 
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getProductValue() {
+        return productValue;
+    }
+
+    public void setProductValue(Double productValue) {
+        this.productValue = productValue;
+    }
+
+
+
+
+
+
+
 
 
 
