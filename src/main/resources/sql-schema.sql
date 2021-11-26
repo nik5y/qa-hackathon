3c980eb75bd8022d34b97e1403256e3b6773c465
E Vas <57002107+elpi-gl@users.noreply.github.com>
2021-11-26 13:27:07 +0000

    Update sql-schema.sql

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -30,3 +30,20 @@ CREATE TABLE IF NOT EXISTS  `ims`.`order` (
     REFERENCES `ims`.`customers` (`id`)
 	ON UPDATE CASCADE ON DELETE CASCADE);
 	
+CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
+    `order_id` INT NOT NULL,
+    `item_id` INT NOT NULL,
+	`item_name` VARCHAR(45) NOT NULL,
+	`item_price` DECIMAL NOT NULL,
+    PRIMARY KEY (`order_id` , `item_id`),
+    CONSTRAINT `order_id` FOREIGN KEY (`order_id`) REFERENCES `ims`.`order` (`order_id`)
+	ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `ims`.`items` (`id`)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `item_name`
+	FOREIGN KEY (`item_name`) REFERENCES `ims`.`items` (`item_name`)
+	ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `item_price`
+	FOREIGN KEY (`item_price`) REFERENCES `ims`.`items` (`item_price`)
+	ON UPDATE CASCADE ON DELETE CASCADE
+);
